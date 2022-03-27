706d81505b73d272a10362a0e31e828c53bb5037
AndresSalomon1990 <a.salomon.1990@gmail.com>
2022-03-27 19:31:55 -0300

    Initialize firebase and firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB329zb2GRlQUzRMbAZYkLdTe73EVvpOPc',
+  authDomain: 'reading-list-app-4d615.firebaseapp.com',
+  projectId: 'reading-list-app-4d615',
+  storageBucket: 'reading-list-app-4d615.appspot.com',
+  messagingSenderId: '607643365523',
+  appId: '1:607643365523:web:1b5ad586f6c9aaf84f9d66',
+};
+
+// init firebase
+initializeApp(firebaseConfig);
+
+// init firestore
+const db = getFirestore();
+
+export { db };
