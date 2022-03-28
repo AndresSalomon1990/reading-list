5ab7fab29fb26040ca7af7622b2867419a86d247
AndresSalomon1990 <a.salomon.1990@gmail.com>
2022-03-28 20:37:07 -0300

    Create useLogout hook to log users out

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,13 @@
+// firebase imports
+import { auth } from '../firebase/config';
+import { signOut } from 'firebase/auth';
+
+export const useLogout = () => {
+  const logout = () => {
+    signOut(auth)
+      .then(() => console.log('User signed out'))
+      .catch((err) => console.log(err.message));
+  };
+
+  return { logout };
+};
